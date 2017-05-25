5ebb5c2f10ebbcf9b053e5d799198f73c397a0ef
Richard Chapman <rchapman@hpccsystems.com>
2017-05-25 15:45:18 +0100

    Split off 6.4.0
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -3,7 +3,7 @@
 ###
 set ( GANGLIA_PROJECT "community" )
 set ( GANGLIA_MAJOR 6 )
-set ( GANGLIA_MINOR 3 )
+set ( GANGLIA_MINOR 5 )
 set ( GANGLIA_POINT 0 )
 set ( GANGLIA_MATURITY "trunk" )
 set ( GANGLIA_SEQUENCE 0 )
