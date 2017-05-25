23a87fedb8e8f56d14e7c7b458678042859d5309
Richard Chapman <rchapman@hpccsystems.com>
2017-05-25 15:44:03 +0100

    Ganglia Monitoring 6.4.0-rc1 Release Candidate 1
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -3,8 +3,8 @@
 ###
 set ( GANGLIA_PROJECT "community" )
 set ( GANGLIA_MAJOR 6 )
-set ( GANGLIA_MINOR 3 )
+set ( GANGLIA_MINOR 4 )
 set ( GANGLIA_POINT 0 )
-set ( GANGLIA_MATURITY "trunk" )
-set ( GANGLIA_SEQUENCE 0 )
+set ( GANGLIA_MATURITY "rc" )
+set ( GANGLIA_SEQUENCE 1 )
 ###
