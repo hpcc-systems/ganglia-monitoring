b03d69b468fe49ae8d93e35500cda6e2de22a7e7
Richard Chapman <rchapman@hpccsystems.com>
2017-09-27 17:11:17 +0100

    Ganglia Monitoring 6.4.2-1 Gold
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 set ( GANGLIA_PROJECT "community" )
 set ( GANGLIA_MAJOR 6 )
 set ( GANGLIA_MINOR 4 )
-set ( GANGLIA_POINT 0 )
+set ( GANGLIA_POINT 2 )
 set ( GANGLIA_MATURITY "release" )
 set ( GANGLIA_SEQUENCE 1 )
 ###
