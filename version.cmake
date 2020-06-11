2cb73ffe22158104cf15b51ca36a99824610ba87
Richard Chapman <rchapman@hpccsystems.com>
2020-06-11 13:42:38 +0100

    Split off 7.6.52
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -5,7 +5,7 @@ set ( HPCC_NAME "Ganglia Monitoring" )
 set ( HPCC_PROJECT "ganglia-monitoring" )
 set ( HPCC_MAJOR 7 )
 set ( HPCC_MINOR 6 )
-set ( HPCC_POINT 51 )
+set ( HPCC_POINT 53 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
